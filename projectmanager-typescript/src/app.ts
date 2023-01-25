41fe9df06fe5eec58e1eead1c35dc85688aba657
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2023-01-25 12:26:41 +0200

    update

--- projectmanager-typescript/src/app.ts
+++ projectmanager-typescript/src/app.ts
@@ -5,6 +5,10 @@ class ProjectInput {
      hostElement: HTMLDivElement;
      element: HTMLFormElement;
 
+     titleInputElement: HTMLInputElement;
+     descriptionInputElement: HTMLInputElement;
+     peopleInputElement: HTMLInputElement;
+
      constructor() {
         this.templateElement = document.querySelector('#project-input')! as HTMLTemplateElement;
         this.hostElement = document.querySelector('#app')! as HTMLDivElement;
@@ -12,7 +16,26 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.attach();
+       
+
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        this.attach();
+        this.configure();
+
      }
+
+     private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+     }
+
+     private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+     }
+
      private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
      }
