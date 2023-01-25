07f268db13538eee241489589620449ae301c951
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2023-01-25 13:43:12 +0200

    updated

--- projectmanager-typescript/src/app.ts
+++ projectmanager-typescript/src/app.ts
@@ -1,5 +1,19 @@
 console.log('PMApp in TS begins...');
 
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor)
+    {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this);
+                return boundFn;
+            },
+        };
+        return adjDescriptor;
+    }
+
+
 class ProjectInput {
      templateElement: HTMLTemplateElement;
      hostElement: HTMLDivElement;
@@ -24,11 +38,35 @@ class ProjectInput {
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
         this.attach();
         this.configure();
+    }
 
-     }
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
 
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input!');
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
      private submitHandler(event: Event) {
         event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
         console.log(this.titleInputElement.value);
      }
 
@@ -39,6 +77,8 @@ class ProjectInput {
      private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
      }
+
+
 }
 
 const proInput = new ProjectInput();
