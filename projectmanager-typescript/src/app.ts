9c56bd292c35ef6be24d75c801b48e2e0d981689
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2023-01-25 12:11:06 +0200

    first commit

---
+++ projectmanager-typescript/src/app.ts
@@ -0,0 +1,21 @@
+console.log('PMApp in TS begins...');
+
+class ProjectInput {
+     templateElement: HTMLTemplateElement;
+     hostElement: HTMLDivElement;
+     element: HTMLFormElement;
+
+     constructor() {
+        this.templateElement = document.querySelector('#project-input')! as HTMLTemplateElement;
+        this.hostElement = document.querySelector('#app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+     }
+     private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+     }
+}
+
+const proInput = new ProjectInput();
